3d0a233ff696b3bf207ee12360ff17c8d103894d
Sujad Syed <trainer@titansystems.org>
2025-12-15 19:03:58 +0530

    test

--- autoscale_demo/tests/integration/test_api.py
+++ autoscale_demo/tests/integration/test_api.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from app.main import app
+from autoscale_demo.app.main import app
 
 client = TestClient(app)
 
--- autoscale_demo/tests/unit/test_db.py
+++ autoscale_demo/tests/unit/test_db.py
@@ -1,4 +1,4 @@
-from app.db import get_data
+from autoscale_demo.app.db import get_data
 
 def test_get_data():
     assert get_data() == "dummy-data"
